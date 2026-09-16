6c7198d8cd04270b01040f0934f6f51c0110ba4f
Vaishu321 <vaishgurunath@gmail.com>
2026-09-16 23:38:23 +0530

    Trying Ruff for formatting

---
+++ Python_for_AI/ruff.py
@@ -0,0 +1,27 @@
+# At first the formatting was like this but saving - Ruff corrected the formatting itself
+
+"""import os
+def   calculate_total(items):
+    total=0
+    for item in items:
+        total+=item['price']*item['quantity']
+    return total
+
+shopping_cart=[{'name':'apple','price':0.5,'quantity':6},{'name':'banana','price':0.3,'quantity':8}]
+print(calculate_total(shopping_cart))"""
+
+import os
+
+
+def calculate_total(items):
+    total = 0
+    for item in items:
+        total += item["price"] * item["quantity"]
+    return total
+
+
+shopping_cart = [
+    {"name": "apple", "price": 0.5, "quantity": 6},
+    {"name": "banana", "price": 0.3, "quantity": 8},
+]
+print(calculate_total(shopping_cart))
