2a4313694d35c189c4edc37314498d7c0b8fffb0
Vaishu321 <vaishgurunath@gmail.com>
2026-09-16 23:24:38 +0530

    Finally clause

--- Python_for_AI/try_and_except.py
+++ Python_for_AI/try_and_except.py
@@ -19,3 +19,13 @@ except ZeroDivisionError:
 # the finally clause
 # Code in finally always runs, error or not:
 
+try:
+    file = open('data.txt', 'r')
+    data = file.read()
+except FileNotFoundError:
+    print("File not found")
+finally:
+    # This always runs to clean up
+    if 'file' in locals() and not file.closed:
+        file.close()
+    print("Cleanup complete")
