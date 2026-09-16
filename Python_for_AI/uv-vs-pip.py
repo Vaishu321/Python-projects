57366f1b7e86a9fa0447eeeaa0c797ef698e7c46
Vaishu321 <vaishgurunath@gmail.com>
2026-09-16 23:44:36 +0530

    More comments

--- Python_for_AI/uv-vs-pip.py
+++ Python_for_AI/uv-vs-pip.py
@@ -20,3 +20,32 @@ venv (virtual environments)
 pip-tools (dependency management)
 pyenv (Python version management)
 Written in Rust, it’s blazingly fast and just works."""
+
+"""pip install pandas
+# ⏱️ Takes 15-30 seconds
+# 
+# 
+# uv pip install pandas
+# ⚡ Takes 1-2 seconds"""
+
+"""Traditional way
+
+# Create virtual environment
+python -m venv .venv
+
+# Activate it (different per OS!)
+# Windows: .venv\Scripts\activate
+# Mac/Linux: source .venv/bin/activate
+
+# Install packages
+pip install requests pandas numpy
+
+# Save dependencies
+pip freeze > requirements.txt"""
+
+
+# Time: ~45 seconds Commands: 4+ (varies by OS)
+
+"""# Everything in one command
+uv init
+uv add requests pandas numpy"""
